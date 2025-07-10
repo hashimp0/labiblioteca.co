2536e55a6111f946361eb40213ac52145a5cea3d
com.hhp <hashimp4u3802@gmail.com>
2025-07-10 10:47:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function handleSearch() {
+  const query = document.getElementById("searchBar").value.trim();
+
+  if (query === "") {
+    alert("Please enter a book name.");
+    return;
+  }
+  const annaUrl = `https://annas-archive.org/search?q=${encodeURIComponent(
+    query
+  )}`;
+  window.open(annaUrl, "_blank");
+}
+document.getElementById("searchButton").addEventListener("click", handleSearch);
+document.getElementById("searchBar").addEventListener("keypress", (event) => {
+  if (event.key === "Enter") {
+    handleSearch();
+  }
+});
