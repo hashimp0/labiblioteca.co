4a5a868e8bb7d76b64810edd096a6cbdcfe366f0
com.hhp <hashimp4u3802@gmail.com>
2025-07-21 06:08:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,12 +5,10 @@ function handleSearch() {
     alert("Please enter a book name.");
     return;
   }
-  const oceanUrl = `https://oceanofpdf.com/?s=${encodeURIComponent(query)}`;
-  const annaUrl = `https://annas-archive.org/search?q=${encodeURIComponent(query)}`;
-
-  window.open(oceanUrl, "_blank");
-  window.open(annaUrl, "_blank");
+  const zlibUrl = `https://z-library.sk/s/${encodeURIComponent(query)}`;
+  window.open(zlibUrl, "_blank");
 }
+
 document.getElementById("searchButton").addEventListener("click", handleSearch);
 document.getElementById("searchBar").addEventListener("keypress", (event) => {
   if (event.key === "Enter") {
